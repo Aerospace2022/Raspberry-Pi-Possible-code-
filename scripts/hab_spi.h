c15af592029f3f14a64863bebe9ff033a62821b0
alan duncan <alan@cocoa-factory.com>
2013-04-21 21:10:24 +0000

    removed c++ version of hab_spi, converting to straight c

--- scripts/hab_spi.h
+++ scripts/hab_spi.h
@@ -1,22 +1,18 @@
 #include <bcm2835.h>
 #include <inttypes.h>
 
-using namespace std;
-
 typedef enum {
     HAB_SPI_CSA,
     HAB_SPI_CSB,
     HAB_SPI_CSC,
     HAB_SPI_CSD
-} HAB_SPI_CS;
+} hab_spi_cs_t;
 
-class HABSPI {
-        HAB_SPI_CS _cs;
-        uint8_t _auxA, _auxB;
-        
-        void _setup(void);
-  public:
-        HABSPI(HAP_SPI_CS aCS, uint8_t A, uint8_t B);
-        void lower_cs(void);
-        void raise_cs(void);
-}
+void hab_spi_set_cs(hab_spi_cs_t aCS);
+void hab_spi_set_aux_gpio(uint8_t pinA, uint8_t pinB);
+void hab_spi_lower_cs(void);
+void hab_spi_raise_cs(void);
+
+hab_spi_cs_t hab_spi_cs(void);
+uint8_t hab_spi_aux_gpio_A(void);
+uint8_t hab_spi_aux_gpio_B(void);
